8c7e65f9dd3e9e437a729ff62bf3ab5d3a995efc
JakubAlbert <133355370+JakubAlbert@users.noreply.github.com>
2023-05-15 21:22:51 +0200

    Delete albert_zadanie directory

--- albert_zadanie/partials/form.php
+++
@@ -1,8 +0,0 @@
-<form id="contact" action="inc/kontakt/insert.php" method="post">
-            <input type="text" name ="meno" placeholder="Vaše meno" id ="meno"  required><br>
-            <input type="email" name ="email" placeholder="Váš email" id="email" required><br>
-            <textarea name="sprava" placeholder="Vaša správa" id="sprava"></textarea><br>
-            <input type="checkbox" name="" id="" required><label for=""> Súhlasím so spracovaním osobných údajov.</label><br>
-            <input type="submit" value="Odoslať", name="insert_kontakt">
-</form>
-<div id="error" class="text-red">
--- albert_zadanie/projekt/index.php
+++
@@ -1,8 +0,0 @@
-<?php include('partials/header.php') ?>
-
-            <?php include('partials/about.php') ?>
-            <?php include('partials/timeline.php') ?>
-            <?php include('partials/reviews.php') ?>
-            <?php include('partials/book.php') ?>
-
-<?php include('partials/footer.php') ?>
